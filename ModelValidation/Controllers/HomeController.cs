df440509050d31b75ecf975108cf7b285e653bad
tonousa <tonousa@hotmail.com>
2015-08-18 09:17:35 -0400

    616

---
+++ ModelValidation/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ModelValidation.Models;
+
+namespace ModelValidation.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult MakeBooking()
+        {
+            return View(new Appointment { Date = DateTime.Now });
+        }
+
+        [HttpPost]
+        public ViewResult MakeBooking(Appointment appt)
+        {
+            // Store appointment in repository
+            return View("Completed", appt);
+        }
+    }
+}
---
+++ ModelValidation/Models/Appointment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace ModelValidation.Models
+{
+    public class Appointment
+    {
+        public string ClientName { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+
+        public bool TermsAccepted { get; set; }
+    }
+}
